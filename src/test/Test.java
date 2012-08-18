e9cc951024da9944e06114f5ac4b3d2afd2010ee
Kohei Imatomi <ondatyan@gmail.com>
2012-08-18 16:08:43 +0900

    format
    Signed-off-by: Kohei Imatomi <ondatyan@gmail.com>

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,20 +2,24 @@ package test;
 
 public class Test {
 	private String pass;
-	public void test(){
-		System.out.println(pass);
-	}
-	public Test(String pass){
-		this.pass = pass;
-	}
-	public void printTest(){
+
+	public void test() {
 		System.out.println(pass);
 	}
 
-	public void printUoooooo(){
+	public Test(String pass) {
+		this.pass = pass;
+	}
+
+	public void printTest() {
+		System.out.println(pass);
+	}
+
+	public void printUoooooo() {
 		System.out.println("uooooooooooooo");
 	}
-	public void testConflict(){
+
+	public void testConflict() {
 		System.out.println("test");
 	}
 }
