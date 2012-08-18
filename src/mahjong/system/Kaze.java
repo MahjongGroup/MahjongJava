a7b7e8cef91cd5010df79d7137edbbb130405193
kotaro <kotaro@kotaro-PC>
2012-08-18 17:00:31 +0900

    kaze class added
    Signed-off-by: kotaro <kotaro@kotaro-PC>

---
+++ src/mahjong/system/Kaze.java
@@ -0,0 +1,23 @@
+package mahjong.system;
+
+public enum Kaze {
+	TON(0),NAN(1),SHA(2),PE(3);
+	
+	private final int kaze;
+	
+	Kaze(int kaze){
+		this.kaze = kaze;
+	}
+	
+	int getNextKaze(){
+		if(0 <= kaze && kaze <= 2){
+			return kaze+1;
+		}
+		return 0;
+	}
+	
+	int getKazeType(){
+		return kaze;
+	}
+	
+}
