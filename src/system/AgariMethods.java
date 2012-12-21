58237f8f1e5b693e81febc72c645a410fd6fd7bd
fillshion <fillshion@gmail.com>
2012-12-21 18:28:42 +0900

    furiten staticalized

--- src/system/AgariMethods.java
+++ src/system/AgariMethods.java
@@ -199,7 +199,7 @@ public class AgariMethods {
 	 * @param sutehaiList 捨てた牌のリスト.
 	 * @return フリテンの場合はtrue.
 	 */
-	public boolean isFuriten(List<Hai> machiList, List<Hai> sutehaiList) {
+	public static boolean isFuriten(List<Hai> machiList, List<Hai> sutehaiList) {
 		for (Hai hai : machiList) {
 			if(sutehaiList.contains(hai))
 				return true;
@@ -217,7 +217,7 @@ public class AgariMethods {
 	 * @param naki 鳴いている場合true. 
 	 * @return 待ち牌リスト.
 	 */
-	public List<Hai> getMachiHaiList(TehaiList list, boolean naki) {
+	public static List<Hai> getMachiHaiList(TehaiList list, boolean naki) {
 		List<Hai> machiHaiList = new ArrayList<Hai>();
 		for (Hai hai : MajanHai.values()) {
 			if(isKeisikiAgari(list, naki, hai))
