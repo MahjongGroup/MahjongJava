a6229b9e80c8cf6f1d9103639e373512d8dc9b2d
kohei <kohei@kohei-PC-VY10ACZE3.(none)>
2013-05-31 23:33:16 +0900

    client gui modified

---
+++ src/test/client/gui/Constant.java
@@ -0,0 +1,10 @@
+package test.client.gui;
+
+public class Constant {
+	public static final int TEHAI_X = 100;
+	public static final int TEHAI_Y = 300;
+	public static final int TSUMO_W = 20;
+	
+	public static final int HAI_W = 50;
+	public static final int HAI_H = 75;
+}
